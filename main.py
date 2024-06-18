113f16c46c2ea56a2e64ebb0f673a05ec7de0b0f
Victory Ma <victoryma23@gmail.com>
2024-06-17 20:33:43 -0500

    Main python file

---
+++ main.py
@@ -0,0 +1,11 @@
+name = 'Victor'
+age = 20
+print(name,'is',age,'years old.')
+
+print('Dear Mina,\nI miss you.')
+
+print ('He said, \"Wow, that\'s crazy!\"')
+
+print("HELLo".isupper())
+
+name = input("Please enter your name: ")
